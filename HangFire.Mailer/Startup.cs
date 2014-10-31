7ea17d34992fa566ad9485fa22d051e2e5e80199
Aviv Shafir <aviv@optimalfinance.co.il>
2014-10-31 16:35:59 +0200

    init

---
+++ HangFire.Mailer/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangFire.Mailer.Startup))]
+
+namespace HangFire.Mailer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfire(config =>
+            {
+                // Basic setup required to process background jobs.
+                config.UseSqlServerStorage("MailerDb");
+                config.UseServer();
+            });
+        }
+    }
+}
